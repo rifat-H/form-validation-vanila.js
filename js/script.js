4ac17306fa2e2cafac25238d1171e73533cef5fc
Rifat Hossain <rifath.h@hotmail.com>
2020-10-24 08:08:38 +0600

    check length and match string added

--- js/script.js
+++ js/script.js
@@ -15,18 +15,20 @@ function validate(formData) {
   let confirm_p = formData["confirm_p"];
 
   // validate empty all
-
   isBlankAll([username, email, password, confirm_p]);
 
   // validate username
+  checkLength(username, 3, 12);
 
   // validate email
   validateEmail(email);
-    
 
   // validate password
+  checkLength(password, 3, 25);
 
   // validate confirm_p
+  checkLength(confirm_p, 3, 25);
+  matchString(password, confirm_p);
 }
 
 //
@@ -42,6 +44,28 @@ function validate(formData) {
 
 // validate functions
 
+function matchString(p1, p2) {
+  if (p1.value !== p2.value) {
+    showError(p2, `${p2.id.toUpperCase()} does not match with ${p1.id.toUpperCase()}`);
+  }
+}
+
+function checkLength(el, min, max = 0) {
+  if (el.value.length < min) {
+    showError(
+      el,
+      `${el.id.toUpperCase()} cannot be less than ${min} characters`
+    );
+  } else if (el.value.length > max) {
+    showError(
+      el,
+      `${el.id.toUpperCase()} cannot be more than ${max} characters`
+    );
+  } else {
+    showSuccess(el);
+  }
+}
+
 function validateEmail(el) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   let valid = re.test(String(el.value).toLowerCase());
@@ -55,11 +79,11 @@ function validateEmail(el) {
 
 function isBlankAll(elArr) {
   elArr.forEach(function (el) {
-    is_blank(el);
+    isBlank(el);
   });
 }
 
-function is_blank(el) {
+function isBlank(el) {
   if (el.value === "") {
     showError(el, `${el.id.toUpperCase()} is required`);
   } else {
